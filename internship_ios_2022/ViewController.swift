b3a67fbb42675bdb1e7f61343d95c7b287538c60
Naughty Squire <naughtysquiredev@icloud.com>
2022-11-07 12:55:30 +0300

    Initial Commit

---
+++ internship_ios_2022/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  internship_ios_2022
+//
+//  Created by Роман Олегович on 07.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
