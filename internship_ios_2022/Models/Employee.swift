26adb6f6357c70b159f7dd46f91e64ecbe7a45e2
Naughty Squire <naughtysquiredev@icloud.com>
2022-11-07 19:14:04 +0300

    EmployeesTable completed

--- internship_ios_2022/Models/Employee.swift
+++ internship_ios_2022/Models/Employee.swift
@@ -2,3 +2,9 @@ struct Employee: Decodable {
     let name, phoneNumber: String
     let skills: [String]
 }
+
+extension Employee: Comparable {
+    static func < (lhs: Employee, rhs: Employee) -> Bool {
+        lhs.name < rhs.name
+    }
+}
